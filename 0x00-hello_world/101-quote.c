014e4e796a1eefc0878cd9b96491578c60eba5cd
girliehappy <etetimh@gmail.com>
2023-04-23 13:30:32 -0700

    sentence

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Print a sentence
+ * Return: Always 1 (success)
+ */
+int main(void)
+{
+	echo("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
