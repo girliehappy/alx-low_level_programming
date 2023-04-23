76d463d8df8068ff542c1c1078df3b913c117c25
girliehappy <etetimh@gmail.com>
2023-04-23 14:05:19 -0700

    updated sentence

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
- * main - Entry point
+ * main - EMpty point
  * Description: Print a sentence
  * Return: Always 1 (success)
  */
 int main(void)
 {
-	echo("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
-	return (1);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
