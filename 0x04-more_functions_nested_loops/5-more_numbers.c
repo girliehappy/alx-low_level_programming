d1e250017d10aaf3d49e9083309ce22f1a942186
girliehappy <etetimh@gmail.com>
2023-04-28 12:47:49 -0700

    Print numbers from 0 to 14, up to ten times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,17 +7,15 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 	for (j = 0; j <= 14; j++)
 	{
 	if (j >= 10)
 	{
 		_putchar((j / 10) + '0');
+	}
 		_putchar((j % 10) + '0');
-		continue;
-		}
-	_putchar('0' + j);
 	}
 	_putchar('\n');
 	}
