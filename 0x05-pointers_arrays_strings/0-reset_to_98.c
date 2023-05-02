5733a170d69c3be4b382c02f1fc1b3f976b56b22
girliehappy <etetimh@gmail.com>
2023-05-02 08:32:18 -0700

    Changing an existing value of *n to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - A function that takes a pointer to an int as parameter
+ * and updates the value it points to to 98
+ * @n: character to be printed out
+ * Return: n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,3 +5,6 @@
 #include <unistd.h>
 
 int _putchar(char c);
+void reset_to_98(int *n);
+
+#endif
