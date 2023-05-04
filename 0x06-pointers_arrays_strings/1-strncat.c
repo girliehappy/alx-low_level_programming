a4d7b72edbe04a413259b227178632c2dd4483e2
girliehappy <etetimh@gmail.com>
2023-05-04 14:21:16 -0700

    A function that concatenates two values. It will make use of most of the n bytes from src

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _strncat - A function that concatenate two strings
+ * _strncat - A function that concatenates two strings
  * It will be using at most n bytes from src
  * @dest: input value
  * @src: input value
@@ -19,12 +19,12 @@ char *_strncat(char *dest, char *src, int n)
 		e++;
 	}
 	m = 0;
-	/*while (m < n && src[m] != '\0')
+	while (m < n && src[m] != '\0')
 	{
 	dest[e] = src[m];
 	e++;
 	m++;
-	}*/
+	}
 	dest[e] = '\0';
 	return (dest);
 }
