600d79382a618c9f552e5b6c72bf6a6394110938
girliehappy <etetimh@gmail.com>
2023-05-22 12:36:49 -0700

    A header file that defines a macro named SIZE as an abbreviation for the token 1024

---
+++ 0-object_like_macro.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * a header file that defines a macro named SIZE as an abbreviation
+ * for the token 1024
+ * Return: Always 0 (success)
+ */
+	#define SIZE 1024
