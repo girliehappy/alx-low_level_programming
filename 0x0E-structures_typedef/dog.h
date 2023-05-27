c415f69f3792bda7ce71e02bddb2b38a4482b5b5
girliehappy <etetimh@gmail.com>
2023-05-27 07:04:44 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - The basic info of a dog
+ * @name: First element of the dog
+ * @age: Second element of the dog
+ * @owner: Third element of the dog
+ * Return: 0
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
